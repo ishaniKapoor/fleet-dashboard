db490ba903bccc1ead7f91697be083931a2fb997
Ishani Kapoor <ishani_kaps@hotmail.com>
2026-06-29 14:24:58 -0400

    small change

--- scripts/migrate_fleet.py
+++ scripts/migrate_fleet.py
@@ -1,6 +1,23 @@
 import json
+from pymongo import MongoClient
 
 with open('../data/fleet-data.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-print(data)
+uri="mongodb+srv://ishanikaps_db_user:<db_password>@fleetnodeproject.ccurbbt.mongodb.net/?retryWrites=true&w=majority"
+
+try:
+    client = MongoClient(uri)
+    db = client["Fleet"]
+    collection = db["fleets"]
+
+    client.admin.command('ping')
+    print("Pinged your deployment. Successfully connected to mongoDB!")
+
+    test_doc = data
+    result = collection.insert_one(test_doc)
+    print(f"Inserted doc id: {result.inserted_id}")
+except Exception as e:
+    print(f"Connection failed: {e}")
+finally:
+    client.close()
